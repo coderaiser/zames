82321046645909fd69a73ae74b5bcfc8965ede2d
coderaiser <coderaiser@cloudcmd.io>
2018-05-03 19:29:14 +0300

    feature(zames) increased arguments count to 8

--- test/zames.js
+++ test/zames.js
@@ -10,9 +10,9 @@ test('zames: arguments: no', (t) => {
 
 test('zames: arguments: fn: count', (t) => {
     /*eslint no-unused-vars: 0 */
-    const fn = () => zames((a, b, c, d, e) =>{});
+    const fn = () => zames((a, b, c, d, e, f, g, h, i) =>{});
     
-    t.throws(fn, /fn takes to much arguments, up to 4 supported/, 'should throw when to much arguments');
+    t.throws(fn, /fn takes to much arguments, up to 8 supported/, 'should throw when to much arguments');
     t.end();
 });
 
@@ -84,6 +84,105 @@ test('zames: promisify + currify: 3 arguments: rejects', (t) => {
     promise('hello', 'world', '!')
         .catch((e) => {
             t.equal(e.message, 'hello', 'should reject');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 4 arguments: resolves', (t) => {
+    const promise = zames((a, b, c, d, fn) => {
+        fn(null, `${a} ${b} ${c}${d}`);
+    });
+    
+    promise('hello', 'amazing', 'world', '!')
+        .then((a) => {
+            t.equal(a, 'hello amazing world!', 'should resolve');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 4 arguments: rejects', (t) => {
+    const promise = zames((a, b, c, d, fn) => {
+        fn(Error(a));
+    });
+    
+    promise('hello', 'amazing', 'world', '!')
+        .catch((e) => {
+            t.equal(e.message, 'hello', 'should reject');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 5 arguments: resolves', (t) => {
+    const promise = zames((a, b, c, d, e, fn) => {
+        fn(null, a + b + c + d + e);
+    });
+    
+    promise(1, 2, 3, 4, 5)
+        .then((a) => {
+            t.equal(a, 15, 'should resolve');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 5 arguments: rejects', (t) => {
+    const msg = 'error'
+    const promise = zames((a, b, c, d, e, fn) => {
+        fn(Error(msg));
+    });
+    
+    promise(1, 2, 3, 4, 5)
+        .catch((e) => {
+            t.equal(e.message, msg, 'should reject');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 6 arguments: resolves', (t) => {
+    const promise = zames((a, b, c, d, e, f, fn) => {
+        fn(null, a + b + c + d + e + f);
+    });
+    
+    promise(1, 2, 3, 4, 5, 6)
+        .then((a) => {
+            t.equal(a, 21, 'should resolve');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 6 arguments: rejects', (t) => {
+    const msg = 'error'
+    const promise = zames((a, b, c, d, e, f, fn) => {
+        fn(Error(msg));
+    });
+    
+    promise(1, 2, 3, 4, 5, 6)
+        .catch((e) => {
+            t.equal(e.message, msg, 'should reject');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 7 arguments: resolves', (t) => {
+    const promise = zames((a, b, c, d, e, f, g, fn) => {
+        fn(null, a + b + c + d + e + f + g);
+    });
+    
+    promise(1, 2, 3, 4, 5, 6, 7)
+        .then((a) => {
+            t.equal(a, 28, 'should resolve');
+            t.end();
+        });
+});
+
+test('zames: promisify + currify: 7 arguments: rejects', (t) => {
+    const msg = 'error'
+    const promise = zames((a, b, c, d, e, f, g, fn) => {
+        fn(Error(msg));
+    });
+    
+    promise(1, 2, 3, 4, 5, 6, 7)
+        .catch((e) => {
+            t.equal(e.message, msg, 'should reject');
             t.end();
         });
 });
